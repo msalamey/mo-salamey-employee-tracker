3adec2a84db49d6cdd83df7d7cf2f7ef25d504b5
msalamey <m.salamey23@gmail.com>
2022-10-09 14:18:48 -0400

    added schema/seeds in employees.sql

---
+++ employees.sql
@@ -0,0 +1,51 @@
+DROP DATABASE IF EXISTS employees_db; 
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(40) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(40) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT, 
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR (40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  role_id INT NOT NULL, 
+  manager_id INT, 
+  PRIMARY KEY (id)
+);
+
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Human Resources"), ("Legal"), ("Finance");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("CEO", "200000", "5"), ("Software Developer", "100000", "2"), ("Lawyer", "80000", "3"), ("HR Rep", "60000", "4"), ("Salesperson", "40000", "1");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jordan", "Belfort", "1"), ("Steve", "Jobs", "2", "1"), ("Mo", "Salamey", "3", "1"), ("Steve", "Smith", "4", "3"), ("Joe", "Jackson", "5", "1");
+-- Query for view all --
+SELECT e.id, e.first_name, e.last_name, d.name AS department, r.title, r.salary, CONCAT_WS(" ", m.first_name, m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id ORDER BY e.id ASC;
+
+-- Query for view all roles --
+SELECT  r.id, r.title, r.salary, d.name as Department_Name FROM role AS r INNER JOIN department AS d ON r.department_id = d.id;
+
+--Query for getting employees --
+SELECT id, CONCAT_WS(' ', first_name, last_name) AS Employee_Name FROM employee
+
+-- Query for updating --
+UPDATE employee SET role_id = 3 WHERE id = 8;
+UPDATE employee SET ? WHERE ?;
+
+-- Query for Delete --
+DELETE FROM department WHERE id = 13; 
