c0a47ed5468154b3199e797ae9195186c3ad39a0
msalamey <m.salamey23@gmail.com>
2022-10-09 21:48:10 -0400

    corrected mysql pkg

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const mysql = require ('mysql'); 
+const mysql = require ('mysql2'); 
 const inquirer = require('inquirer');
 const { printTable } = require('console-table-printer');
 const text = require('text');
