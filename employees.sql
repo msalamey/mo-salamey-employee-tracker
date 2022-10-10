f017976ce21fb5f9b731f4897daf4d61e3a4e801
msalamey <m.salamey23@gmail.com>
2022-10-09 21:32:12 -0400

    made changes to correct sql error

--- employees.sql
+++ employees.sql
@@ -30,18 +30,18 @@ INSERT INTO department (name)
 VALUES ("Sales"), ("Engineering"), ("Human Resources"), ("Legal"), ("Finance");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("CEO", "200000", "5"), ("Software Developer", "100000", "2"), ("Lawyer", "80000", "3"), ("HR Rep", "60000", "4"), ("Salesperson", "40000", "1");
+VALUES ("CEO", "200000", 5), ("Software Developer", "100000", 2), ("Lawyer", "80000", 3), ("HR Rep", "60000", 4), ("Salesperson", "50000", 1);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jordan", "Belfort", "1"), ("Steve", "Jobs", "2", "1"), ("Mo", "Salamey", "3", "1"), ("Steve", "Smith", "4", "3"), ("Joe", "Jackson", "5", "1");
+VALUES ("Jordan", "Belfort", 1, null), ("Steve", "Jobs", 2, 1), ("Mo", "Salamey", 3, 1), ("Steve", "Smith", 4, 3), ("Joe", "Jackson", 5, 1);
 -- Query for view all --
 SELECT e.id, e.first_name, e.last_name, d.name AS department, r.title, r.salary, CONCAT_WS(" ", m.first_name, m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id ORDER BY e.id ASC;
 
 -- Query for view all roles --
 SELECT  r.id, r.title, r.salary, d.name as Department_Name FROM role AS r INNER JOIN department AS d ON r.department_id = d.id;
 
---Query for getting employees --
-SELECT id, CONCAT_WS(' ', first_name, last_name) AS Employee_Name FROM employee
+-- Query for getting employees --
+SELECT id, CONCAT_WS(' ', first_name, last_name) AS Employee_Name FROM employee; 
 
 -- Query for updating --
 UPDATE employee SET role_id = 3 WHERE id = 8;
